3255cfdf1b6f41e69dd5a9782b849266f4f58788
Ouchto <mustapha.enaa@gmail.com>
2024-12-10 11:18:30 +0100

    la fonction d'addition

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Entrez le premiere nombre : ");
+        int n1 = input.nextInt();
+        System.out.print("Entrez le dexieme nombre : ");
+        int n2 = input.nextInt();
+        Somme(n1,n2);
+
+    }
+    // la fonction d'addition
+    public static void Somme(int a, int b)
+    {
+        int somme;
+        somme = a + b;
+        System.out.println("la somme est : " + somme);
+    }
+
+}

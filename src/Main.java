e88afedeb68cb5089a2d35374358126a3d5cff77
Ouchto <mustapha.enaa@gmail.com>
2024-12-10 15:09:08 +0100

    la fonction de la Puissance et Switch

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Entrez le premiere nombre : ");
-        int n1 = input.nextInt();
-        System.out.print("Entrez le dexieme nombre : ");
-        int n2 = input.nextInt();
+        System.out.println("Choisissez une opération :");
+        System.out.println("1. Addition ");
+        System.out.println("2. Soustraction ");
+        System.out.println("3. Multiplication ");
+        System.out.println("4. Division ");
+        System.out.println("5. Puissance ");
+        System.out.println("6. Racine carrée ");
+        System.out.println("7. Factorielle ");
 
+        System.out.print("Entrez le choix : ");
+        int choix = input.nextInt();
 
-        Somme(n1,n2);
-        Soustra(n1,n2);
-        Produit(n1,n2);
-        Division(n1,n2);
+        switch (choix) {
+            case 1:
+                System.out.print("Entrez le premiere nombre : ");
+                int n1 = input.nextInt();
+                System.out.print("Entrez le dexieme nombre : ");
+                int n11 = input.nextInt();
+                Somme(n1, n11);
+                break;
+            case 2:
+                System.out.print("Entrez le premiere nombre : ");
+                int n2 = input.nextInt();
+                System.out.print("Entrez le dexieme nombre : ");
+                int n22 = input.nextInt();
+                Soustra(n2, n22);
+                break;
+            case 3:
+                System.out.print("Entrez le premiere nombre : ");
+                int n3 = input.nextInt();
+                System.out.print("Entrez le dexieme nombre : ");
+                int n33 = input.nextInt();
+                Produit(n3, n33);
+                break;
+            case 4:
+                System.out.print("Entrez le premiere nombre : ");
+                int n4 = input.nextInt();
+                System.out.print("Entrez le dexieme nombre : ");
+                int n44 = input.nextInt();
+                Division(n4, n44);
+                break;
+            case 5:
+                System.out.print("Entrez le premiere nombre : ");
+                int n5 = input.nextInt();
+                System.out.print("Entrez le dexieme nombre : ");
+                int n55 = input.nextInt();
+                Puissance(n5, n55);
+                break;
+            default:
+                System.out.println("Choix invalide !");
+        }
 
 
     }
+
 
     // la fonction d'addition
     public static void Somme(int a, int b)
@@ -54,6 +96,15 @@ public class Main {
             System.out.println("erreur: division par 0 ");
         }
 
+    }
+    // la fonction de Puissance
+    public static void Puissance(int a, int b)
+    {
+        int puissance = 1;
+        for (int i = 1; i <= b; ++i){
+            puissance *= a;
+        }
+        System.out.println("La puissance est : " + puissance);
     }
 
 
